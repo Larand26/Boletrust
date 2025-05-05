124e6a42419525e6b9e89459f3ba1e14e61eb92a
Victor Laranjeira <vbl09012003vbl@gmail.com>
2025-05-05 18:55:47 -0300

    feat(back): adiciona um endpoint img-verify

---
+++ BoletrustBack/src/main/java/com/USJT/BoletrustBack/Controller/ImgController.java
@@ -0,0 +1,27 @@
+package com.USJT.BoletrustBack.Controller;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.USJT.BoletrustBack.models.Image;
+
+@RestController
+@RequestMapping("/img-verify")
+public class ImgController {
+
+    @CrossOrigin(origins = "*")
+    @PostMapping
+    public HashMap<String, String> uploadImage(@RequestBody Image image) {
+
+        HashMap<String,String> result = new HashMap<>();
+        result.put("message", "foi");
+        return result;
+    }
+    
+    
+}
---
+++ BoletrustBack/src/main/java/com/USJT/BoletrustBack/models/Image.java
@@ -0,0 +1,13 @@
+package com.USJT.BoletrustBack.models;
+
+public class Image {
+    private String img;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
